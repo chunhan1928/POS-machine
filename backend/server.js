e537b3fc4800a010c8ce1a59bce4ae9dce9d515b
b07902059 <45457598+b07902059@users.noreply.github.com>
2022-01-14 11:36:10 +0800

    database init

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from 'mongoose';
 import dotenv from "dotenv-defaults";
 import app from './Route'
+import {dataInit} from './upload'
 
 // connect mongo db
 dotenv.config();
@@ -11,6 +12,10 @@ mongoose.connect(
     useUnifiedTopology: true
 });
 const db = mongoose.connection;
+db.once('open', () => {
+    console.log('MongoDB connected!')
+    dataInit();
+});
 
 // create server
 const port = process.env.PORT || 5000
@@ -18,4 +23,3 @@ app.listen(port, () => {
     console.log
     (`Server listening on: http://localhost:${port}`);
 })
-    
---
+++ backend/upload.js
@@ -0,0 +1,50 @@
+import orderModel from "./Schema/Order";
+import stockModel from "./Schema/Stock";
+import userModel from "./Schema/User";
+
+const orderExample = [
+    // {date: Date(), name: "", cost: 0, price: 0, amount: 0}
+    {date: new Date("2021-12-02T05:51:04.360Z"), name: "珍奶", cost: 30, price: 60, amount: 2},
+    {date: new Date("2021-12-01T03:32:10.360Z"), name: "燙青菜", cost: 20, price: 30, amount: 1}
+]
+const stockExample = [
+    // {name: "", category: "", cost: 0, price: 0, amount: 0}
+    {name: "珍奶", category: "飲料", cost: 30, price: 60, amount: 7},
+    {name: "滷肉飯", category: "飯", cost: 15, price: 30, amount: 30},
+    {name: "滷蛋", category: "小菜", cost: 5, price: 15, amount: 3},
+    {name: "魯菜盤", category: "小菜", cost: 20, price: 50, amount: 30},
+    {name: "燙青菜", category: "小菜", cost: 20, price: 30, amount: 25},
+    {name: "大魯麵", category: "麵", cost: 40, price: 65, amount: 77},
+    {name: "嘴邊肉", category: "小菜", cost: 40, price: 75, amount: 4},
+    {name: "荷包蛋", category: "小菜", cost: 7, price: 15, amount: 50},
+    {name: "炒飯", category: "飯", cost: 45, price: 70, amount: 24}
+]
+const userExample = [
+    // {name: "", password: "", priviledge: ""}
+    {name: "admin", password: "0000", priviledge: "manager"},
+    {name: "clerk1", password: "haha", priviledge: "clerk"},
+    {name: "clerk2", password: "QQ", priviledge: "clerk"}
+]
+
+const dataInit = async () => {
+    const checkOrder = await orderModel.find();
+    const checkStock = await stockModel.find();
+    const checkUser = await userModel.find();
+    if (checkOrder.length !== 2 && process.env.TEST === "true") {
+        await orderModel.deleteMany({});
+        await orderModel.insertMany(orderExample);
+        console.log("order database initialized!");
+    }
+    if(checkStock.length !== 9 && process.env.TEST === "true") {
+        await stockModel.deleteMany({});
+        await stockModel.insertMany(stockExample);
+        console.log("stock database initialized!");
+    }
+    if(checkUser.length !== 3 && process.env.TEST === "true") {
+        await userModel.deleteMany({});
+        await userModel.insertMany(userExample);
+        console.log("user database initialized!");
+    }
+};
+  
+export { dataInit };
